1351cee9ce1d2633dc974baba0bae297797e8530
s-conroy <sierraconroy1@gmail.com>
2024-02-05 23:50:11 -0500

    Basket and destroy

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+    public Text scoreGT; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGO.GetComponent<Text>();
+        scoreGT.text = "0";
     }
 
     // Update is called once per frame
@@ -20,4 +25,15 @@ public class Basket : MonoBehaviour
         pos.x = mousePos3D.x;
         this.transform.position = pos;
     }
+
+    void OnCollisionEnter(Collision coll) {
+          GameObject collidedWith = coll.gameObject;
+      if (collidedWith.tag == "Apple") {
+            Destroy(collidedWith);
+       
+     }
+       int score = int.Parse(scoreGT.text);
+       score += 100;
+       scoreGT.text = score.ToString();
+  }
 }
--- Assets/supportScripts/ApplePicker.cs
+++ Assets/supportScripts/ApplePicker.cs
@@ -9,17 +9,19 @@ public class ApplePicker : MonoBehaviour
     public int    numBaskets = 3;
     public float   basketBottomY = -14;
     public float  basketSpacingY = 2f;
-
-
+    public List<GameObject> basketList;
+ 
 // Start is called before the first frame update
     void Start()
-    {
-        for (int i=0; i<numBaskets; i++){
+    {   basketList = new List<GameObject>();
+
+        for (int i=0; i<numBaskets; i++) {
            GameObject tBasketGO = Instantiate(basketPrefab) as GameObject;
            Vector3 pos = Vector3.zero;
            pos.y = basketBottomY + (basketSpacingY *i);
            tBasketGO.transform.position = pos;
-}
+           basketList.Add(tBasketGO);
+       }
     }
 
     // Update is called once per frame
@@ -27,4 +29,16 @@ public class ApplePicker : MonoBehaviour
     {
         
     }
+
+    public void AppleDestroyed() {
+          GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+          foreach (GameObject tGO in tAppleArray) {
+                 Destroy(tGO);
+       }
+   
+    int basketIndex = basketList.Count-1;
+    GameObject tBasketGO = basketList[basketIndex];
+    basketList.RemoveAt( basketIndex );
+    Destroy(tBasketGO);
+       }
 }
