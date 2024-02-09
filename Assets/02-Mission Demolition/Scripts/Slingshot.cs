8813f2ecb7b469fb41af015844b2060bec65037b
s-conroy <sierraconroy1@gmail.com>
2024-02-09 12:55:25 -0500

    cs

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -1,16 +1,21 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Slingshot : MonoBehaviour
-{
+public class Slingshot : MonoBehaviour{
+
     [Header("Set in Inspector")]
     public GameObject prefabProjectile;
+    public float velocityMult = 8f;
+
 
     [Header("Set Dynamically")]
     public GameObject  launchPoint;
     public Vector3     launchPos;
     public GameObject  projectile;
     public bool   aimingMode;
+
+    private Rigidbody projectileRigidbody;
 
     void Awake() {
          Transform launchPointTrans = transform.Find("LaunchPoint");
@@ -28,13 +33,36 @@ public class Slingshot : MonoBehaviour
          //print("Slingshot:OnMouseExit()");
          launchPoint.SetActive(false);
 
-    }
+      }
     void OnMouseDown() {
          aimingMode = true;
          projectile = Instantiate(prefabProjectile) as GameObject;
          projectile.transform.position = launchPos;
-         projectile.GetComponent<Rigidbody>().isKinematic = true;
-  }
+         projectileRigidbody = projectile.GetComponent<Rigidbody>();
+         projectileRigidbody.isKinematic = true;
+         }
+
+    void Update() {
+         if (!aimingMode) return;
+         Vector3 mousePos2D = Input.mousePosition;
+         mousePos2D.z = -Camera.main.transform.position.z;
+         Vector3 mousepos2D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+         Vector3 mouseDelta = mousePos3D-launchPos;
+         float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+         if (mouseDelta.magnitude > maxMagnitude) {
+             mouseDelta.Normalize();
+             mouseDelta *= maxMagnitude;
+            }
+
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+
+        if (Input.GetMouseButtonUp(0)) {
+            aimingMode = false;
+            projectileRigidBody.isKinematic = false;
+            projectileRigidBody.velocity = -mouseDelta * velocityMult;
+            projectile = null;
+         }
+    }
 }
-
-
